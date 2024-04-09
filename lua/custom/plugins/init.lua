8871f8fc6779fbf71db40558c2da642c0772037b
Hieronim <56017121+Hier0nim@users.noreply.github.com>
2024-04-09 23:31:07 +0200

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+require('nvim-treesitter.install').compilers = { 'zig' }
+vim.wo.wrap = false
+vim.opt.tabsto = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
 return {}
