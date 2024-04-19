9cf71dbc8e1602c1f9fd3aa19929962e2c76f6cb
Hieronim <56017121+Hier0nim@users.noreply.github.com>
2024-04-19 08:35:04 +0200

    Update treesitter.lua

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -17,7 +17,9 @@ return {
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
+       
+      -- Prefer git instead of curl in order to improve connectivity in some environments
+      require('nvim-treesitter.install').prefer_git = tru
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
