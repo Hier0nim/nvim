ba50448c3b53ea0eceab33213184f6a58591c64f
Hieronim <56017121+Hier0nim@users.noreply.github.com>
2024-04-19 08:39:03 +0200

    Update filetree.lua

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,14 +1,25 @@
-vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  config = function()
-    require('neo-tree').setup {}
-  end,
+  cmd = 'Neotree',
+  keys = {
+    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
 }
