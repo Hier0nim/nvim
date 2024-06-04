b0be40b7bbaef8cbbf1a680fa63785ff587c844a
HDA <hieronimdaniel@gmail.com>
2024-06-04 13:24:31 +0200

    make it more lazy

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,11 +1,3 @@
--- Create an autocommand for the 'dbout' file type
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'dbout',
-  callback = function()
-    vim.wo.foldenable = false
-  end,
-})
-
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
@@ -18,8 +10,17 @@ return {
     'DBUIAddConnection',
     'DBUIFindBuffer',
   },
+  lazy = true,
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+
+    -- Create an autocommand for the 'dbout' file type
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'dbout',
+      callback = function()
+        vim.wo.foldenable = false
+      end,
+    })
   end,
 }
