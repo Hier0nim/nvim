e5f02b83e365f1e74e7e1206bb847462451ba2b3
Hier0nim <hieronimdaniel@proton.me>
2025-01-27 17:28:18 +0100

    configure snacks.nvim

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -1,26 +1,26 @@
 return {
-  -- {
-  --   'j-hui/fidget.nvim',
-  --   opts = {
-  --     notification = {
-  --       override_vim_notify = true,
-  --     },
-  --   },
-  -- },
-  --
-  -- -- Disable noice.nvim
-  -- {
-  --   'folke/noice.nvim',
-  --   enabled = false,
-  -- },
-  --
-  -- -- Configure snacks.nvim
-  -- {
-  --   'folke/snacks.nvim',
-  --   opts = {
-  --     notifier = {
-  --       enabled = true,
-  --     },
-  --   },
-  -- },
+  {
+    'j-hui/fidget.nvim',
+    opts = {
+      notification = {
+        override_vim_notify = true,
+      },
+    },
+  },
+
+  -- Disable noice.nvim
+  {
+    'folke/noice.nvim',
+    enabled = false,
+  },
+
+  -- Configure snacks.nvim
+  {
+    'folke/snacks.nvim',
+    opts = {
+      notifier = {
+        enabled = true,
+      },
+    },
+  },
 }
