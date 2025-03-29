df10b537f49f8d61f0568fb3bc718432fb3c22d4
Hier0nim <hieronimdaniel@proton.me>
2025-03-29 02:13:31 +0100

    update dontet package

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
               pyright
               ruff
               roslyn-ls
-              dotnetCorePackages.dotnet_9.sdk
+              dotnetCorePackages.dotnet_10.sdk
             ];
           };
 
