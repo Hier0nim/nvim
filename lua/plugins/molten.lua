01f9a92517c938f28b4b169a7bcd2fe8129da0e6
Hier0nim <hieronimdaniel@proton.me>
2025-06-01 19:05:03 +0200

    adjust molten keybinds

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -164,28 +164,19 @@ return {
     build = ':UpdateRemotePlugins',
     init = function()
       vim.g.molten_image_provider = 'image.nvim'
-      -- vim.g.molten_output_win_max_height = 20
       vim.g.molten_auto_open_output = true
       vim.g.molten_auto_open_html_in_browser = true
       vim.g.molten_tick_rate = 200
     end,
     config = function()
-      local init = function()
-        local quarto_cfg = require('quarto.config').config
-        quarto_cfg.codeRunner.default_method = 'molten'
-        vim.cmd [[MoltenInit]]
-      end
-      local deinit = function()
-        local quarto_cfg = require('quarto.config').config
-        quarto_cfg.codeRunner.default_method = 'slime'
-        vim.cmd [[MoltenDeinit]]
-      end
-      vim.keymap.set('n', '<localleader>mi', init, { silent = true, desc = 'Initialize molten' })
-      vim.keymap.set('n', '<localleader>md', deinit, { silent = true, desc = 'Stop molten' })
-      vim.keymap.set('n', '<localleader>mp', ':MoltenImagePopup<CR>', { silent = true, desc = 'molten image popup' })
-      vim.keymap.set('n', '<localleader>mb', ':MoltenOpenInBrowser<CR>', { silent = true, desc = 'molten open in browser' })
-      vim.keymap.set('n', '<localleader>mh', ':MoltenHideOutput<CR>', { silent = true, desc = 'hide output' })
-      vim.keymap.set('n', '<localleader>ms', ':noautocmd MoltenEnterOutput<CR>', { silent = true, desc = 'show/enter output' })
+      vim.keymap.set('n', '<localleader>mi', ':MoltenInit<CR>', { silent = true, desc = 'Initialize the plugin' })
+      vim.keymap.set('n', '<localleader>e', ':MoltenEvaluateOperator<CR>', { silent = true, desc = 'run operator selection' })
+      vim.keymap.set('n', '<localleader>rl', ':MoltenEvaluateLine<CR>', { silent = true, desc = 'evaluate line' })
+      vim.keymap.set('n', '<localleader>rr', ':MoltenReevaluateCell<CR>', { silent = true, desc = 're-evaluate cell' })
+      vim.keymap.set('v', '<localleader>r', ':<C-u>MoltenEvaluateVisual<CR>gv', { silent = true, desc = 'evaluate visual selection' })
+      vim.keymap.set('n', '<localleader>rd', ':MoltenDelete<CR>', { silent = true, desc = 'molten delete cell' })
+      vim.keymap.set('n', '<localleader>oh', ':MoltenHideOutput<CR>', { silent = true, desc = 'hide output' })
+      vim.keymap.set('n', '<localleader>os', ':noautocmd MoltenEnterOutput<CR>', { silent = true, desc = 'show/enter output' })
     end,
   },
   {
