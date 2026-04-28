301e71ccba957def8bdc1df1786bfe610a33ca15
Hieronim Daniel <hieronim.daniel@shijigroup.com>
2026-04-28 14:46:37 +0200

    Clean up LSP keymaps to align with Neovim 0.12 defaults
    - Remove gd, gD, K, leader>rn, leader>ca, leader>D (now built-in defaults)
    - Remap gr->grr and gI->gri to match 0.12 default key names
    - Keep Snacks pickers for grr and gri
    - Remove empty leader>r which-key group

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -25,19 +25,12 @@ function M.on_attach(_, bufnr)
     vim.lsp.buf.format()
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-
-  nmap('gr', Snacks.picker.lsp_references, '[G]oto [R]eferences')
-  nmap('gI', Snacks.picker.lsp_implementations, '[G]oto [I]mplementation')
+  nmap('grr', Snacks.picker.lsp_references, '[G]oto [R]eferences')
+  nmap('gri', Snacks.picker.lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>ds', Snacks.picker.lsp_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', Snacks.picker.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
 
-  nmap('K', vim.lsp.buf.hover, 'Hover documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature documentation')
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove folder')
   nmap('<leader>wl', list_workspace_folders, '[W]orkspace [L]ist folders')
