7c86a50ce38f6c9678a33c1d539d58a5abb4dbc7
Hieronim Daniel <hieronim.daniel@shijigroup.com>
2026-05-26 13:32:03 +0200

    feat(plugins): add mini and snacks modules for editing and workflow
    - Add mini.ai, mini.pairs, mini.move, mini.bracketed, mini.splitjoin,
      mini.cursorword, mini.hipatterns
    - Add snacks bigfile, quickfile, words, gitbrowse, rename, dim, zen
    - Add keymaps for words jump, gitbrowse, rename, zen, dim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,5 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear search highlight' })
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
 vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up' })
 vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result' })
