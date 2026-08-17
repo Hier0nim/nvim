8ff28f9d60f6fbe28fa055c83d8b285574f4ff90
Hier0nim <hieronimdaniel@proton.me>
2026-08-17 15:45:27 +0200

    fix: EasyDotnet test runner mappings, dap-ui loading, add README
    - Override EasyDotnet default test runner mappings to avoid namespace
      conflicts (<leader>tr/td/ta/tp/te for buffer, short keys for runner)
    - Fix nvim-dap-ui loading (packadd nvim-nio before requiring dapui)
    - Replace Lua API calls with :Dotnet commands (run/build/watch/testrunner)
    - Add README documenting plugin management and keymap namespace
    - Pin easy-dotnet plugin to cccd08c (latest breaks using diagnostics)
    - Add nvim-dap-ui and nvim-nio to lock file

--- lua/plugins/dotnet.lua
+++ lua/plugins/dotnet.lua
@@ -9,6 +9,7 @@ return {
     auto_enable = true,
     dep_of = { 'easy-dotnet.nvim' },
     after = function()
+      vim.cmd.packadd('nvim-nio')
       local dapui = require 'dapui'
       dapui.setup()
 
@@ -56,6 +57,28 @@ return {
         test_runner = {
           auto_start_testrunner = true,
           viewmode = 'float',
+          mappings = {
+            -- Buffer integration (source files)
+            run_test_from_buffer = { lhs = '<leader>tr', desc = 'Run test from buffer' },
+            run_all_tests_from_buffer = { lhs = '<leader>ta', desc = 'Run all tests in file' },
+            get_build_errors = { lhs = '<leader>te', desc = 'Get build errors' },
+            peek_stack_trace_from_buffer = { lhs = '<leader>tp', desc = 'Peek stack trace' },
+            debug_test_from_buffer = { lhs = '<leader>td', desc = 'Debug test from buffer' },
+            -- Test runner window
+            debug_test = { lhs = 'd', desc = 'Debug test' },
+            go_to_file = { lhs = 'g', desc = 'Go to file' },
+            run_all = { lhs = 'R', desc = 'Run all tests' },
+            run = { lhs = 'r', desc = 'Run test' },
+            peek_stacktrace = { lhs = 'p', desc = 'Peek stacktrace' },
+            expand = { lhs = 'o', desc = 'Expand' },
+            expand_node = { lhs = 'E', desc = 'Expand node' },
+            collapse_all = { lhs = 'W', desc = 'Collapse all' },
+            close = { lhs = 'q', desc = 'Close test runner' },
+            refresh_testrunner = { lhs = '<C-r>', desc = 'Refresh' },
+            cancel = { lhs = '<C-c>', desc = 'Cancel' },
+            next_failure = { lhs = ']f', desc = 'Next failing test' },
+            prev_failure = { lhs = '[f', desc = 'Previous failing test' },
+          },
         },
         projx_lsp = { enabled = true },
         csproj_mappings = true,
@@ -86,17 +109,15 @@ return {
       vim.keymap.set({ 'n', 'x' }, '<leader>de', function() require('dapui').eval() end, { desc = 'DAP evaluate expression' })
 
       -- Run and build
-      vim.keymap.set('n', '<leader>rr', function() require('easy-dotnet').run_default() end, { desc = 'Run default project' })
-      vim.keymap.set('n', '<leader>rd', function() require('easy-dotnet').run_default_debug() end, { desc = 'Debug default project' })
-      vim.keymap.set('n', '<leader>rb', function() require('easy-dotnet').build_default() end, { desc = 'Build default project' })
-      vim.keymap.set('n', '<leader>rw', function() require('easy-dotnet').watch_default() end, { desc = 'Watch default project' })
-      vim.keymap.set('n', '<leader>rs', function() require('easy-dotnet').stop() end, { desc = 'Stop running job' })
+      vim.keymap.set('n', '<leader>rr', '<cmd>Dotnet run<CR>', { desc = 'Run default project' })
+      vim.keymap.set('n', '<leader>rd', '<cmd>Dotnet run debug<CR>', { desc = 'Debug default project' })
+      vim.keymap.set('n', '<leader>rb', '<cmd>Dotnet build<CR>', { desc = 'Build default project' })
+      vim.keymap.set('n', '<leader>rw', '<cmd>Dotnet watch<CR>', { desc = 'Watch default project' })
+      vim.keymap.set('n', '<leader>rs', '<cmd>Dotnet stop<CR>', { desc = 'Stop running job' })
       vim.keymap.set('n', '<leader>rm', '<cmd>Dotnet<CR>', { desc = 'Dotnet action menu' })
 
       -- Tests
-      vim.keymap.set('n', '<leader>tt', function() require('easy-dotnet').test_runner() end, { desc = 'Toggle test runner' })
-      vim.keymap.set('n', '<leader>tr', function() require('easy-dotnet').test_run_nearest() end, { desc = 'Run nearest test' })
-      vim.keymap.set('n', '<leader>td', function() require('easy-dotnet').test_debug_nearest() end, { desc = 'Debug nearest test' })
+      vim.keymap.set('n', '<leader>tt', '<cmd>Dotnet testrunner<CR>', { desc = 'Toggle test runner' })
     end,
   },
 }
